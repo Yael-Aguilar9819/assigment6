53fc551cedd8d97bb4184f62fe0a97f7f8a86669
Yael Aguilar <yaelgarcia9819@outlook.com>
2020-12-05 14:33:00 -0600

    Added first test

---
+++ src/test/java/assigment6/namesSeparatorTest.java
@@ -0,0 +1,27 @@
+package assigment6;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class namesSeparatorTest {
+
+        /**
+         * Rigorous Test.
+         */
+        // @BeforeEach
+        // void  firstTest() {
+        //     System.out.print("ah");
+        // }
+    
+    
+        @Test
+        void testApp() {
+            assertEquals(1, 1);
+        }
+
+        // public static void main(String[] args) {
+        //     testApp();
+        // }
+}
